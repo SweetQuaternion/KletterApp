041d028acdead54e3454c547ae8be54cef59672b
Mona Philine Komp <mona.komp@web.de>
2026-02-23 10:42:02 +0100

    initial commit

---
+++ Backend/SQL/generate-tables.sql
@@ -0,0 +1,73 @@
+CREATE TABLE hallen(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    adresse text NOT NULL
+);
+
+
+CREATE TABLE wände(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    hallen_id INT NOT NULL,
+    wand_nr INT,
+    sektor text,
+    FOREIGN KEY (hallen_id) REFERENCES hallen(id)
+);
+
+
+CREATE TABLE routen(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    wand_id INT NOT NULL,
+    name text,
+    farbe text NOT NULL,
+    schwierigkeit text,
+    is_toprope boolean DEFAULT false,
+    is_vorstieg boolean DEFAULT false,
+    schrauber text,
+    schraubdatum timestamp,
+    isActive boolean DEFAULT true,
+    FOREIGN KEY (wand_id) REFERENCES wände(id)
+)
+
+
+CREATE TABLE users(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name text NOT NULL,
+    email text NOT NULL UNIQUE,
+    passwort_hash text NOT NULL,
+    bild_url text
+);
+
+CREATE TYPE style_enum AS ENUM ('onsight', 'flash', 'redpoint', 'pinkpoint', 'toprope', 'hangdog', 'attempt');
+CREATE TYPE sicherung_enum AS ENUM ('vorstieg', 'toprope', 'solo');
+
+CREATE TABLE ascents(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    route_id INT NOT NULL,
+    datum timestamp NOT NULL,
+    style style_enum,
+    sicherung sicherung_enum,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (route_id) REFERENCES routen(id)
+)
+
+CREATE TABLE user_routen_status(
+    user_id INT NOT NULL,
+    route_id INT NOT NULL,
+    isFavorit boolean DEFAULT false,
+    isProjekt boolean DEFAULT false,
+    gesch_schwierigkeit text,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (route_id) REFERENCES routen(id)
+)
+
+
+CREATE TABLE kommentare(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    route_id INT NOT NULL,
+    datum timestamp NOT NULL,
+    kommentar_text text NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (route_id) REFERENCES routen(id)
+);
