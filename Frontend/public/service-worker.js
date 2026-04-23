5ab32309e174b0b97d005206c64abe201015eaf1
Mona Philine Komp <mona.komp@web.de>
2026-04-23 13:57:16 +0200

    Working service worker it seems

---
+++ Frontend/public/service-worker.js
@@ -0,0 +1,70 @@
+// ♫♫ You are way-maker, service worker, promise-keeper, that is who you are [...]
+
+// ----------------Helper Funktionen------------------------
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const networkFirst = async (request, event) => {
+  try {
+    const responseFromNetwork = await fetch(request);
+    if (!responseFromNetwork || responseFromNetwork.status !== 200) {
+      throw new Error(
+        "Netzwerkantwort war ungültig: " + responseFromNetwork.status,
+      );
+    }
+    await putInCache(request, responseFromNetwork.clone());
+    console.log("Ich liefere vom Netzwerk: " + event.request.url);
+    return responseFromNetwork;
+  } catch (error) {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      console.log("Ich liefere aus dem Cache:" + event.request.url);
+      return responseFromCache;
+    }
+    return new Response("Halluuuu ich hab kein Internet, sorrii!", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
+// ----------------------------------------------------------
+
+// ----------------Servive Worker Code----------------------
+
+// Wird als allererstes ausgeführt
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(addResourcesToCache(["/", "/index.html"]));
+  console.log("Halluuuu ich hab mich installiert");
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim()); // Adoptiert alle Seiten, damit der Service Worker sofort aktiv ist
+  event.waitUntil(self.registration?.navigationPreload.enable()); // Aktiviert die Navigation Preload API, damit die Seite schneller geladen wird, während der Service Worker aktiviert wird
+  console.log("Halluuuu ich bin aktiviert");
+});
+
+self.addEventListener("fetch", async (event) => {
+  if (
+    event.request.url.includes("extension") || // Ignoriere Anfragen an Browser Extensions, da diese nicht von unserem Service Worker behandelt werden sollten
+    event.request.url.includes("openid-connect") // Ignoriere Anfragen an Keycloak, da diese nicht von unserem Service Worker behandelt werden sollten
+  ) {
+    return;
+  }
+  event.respondWith(networkFirst(event.request, event));
+});
+
+// ----------------------------------------------------------
+
+// Läuft in einem Worker Context, hat daher kein Zugriff auf den DOM
+// Braucht https
+// Wir müssen immer unsere request und response streams klonen, da sie nur einmal gelesen werden können
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -8,26 +8,39 @@ import { fetchUser, queryClient } from "./constants/queries.ts";
 import type { UserResponseDTO } from "./api/model";
 import { UserContext } from "./constants/context.ts";
 
-keycloak
+if ("serviceWorker" in navigator) {
+  // nicht alle Browser unterstützen Service Worker, besonders ältere nicht
+  try {
+    await navigator.serviceWorker.register("/service-worker.js"); // globaler Scope per default
+  } catch (error) {
+    console.error("Service Worker Registrierung fehlgeschlagen:", error);
+  }
+}
+
+let user: UserResponseDTO | null = null;
+
+const authenticated = await keycloak
   .init({
     onLoad: "check-sso",
+    checkLoginIframe: false,
     silentCheckSsoRedirectUri:
       window.location.origin + "/silent-check-sso.html",
   })
-
-  .then(async (authenticated) => {
-    let user: UserResponseDTO | null = null;
-    if (authenticated) {
-      user = await fetchUser(keycloak.subject!, keycloak.tokenParsed?.name!);
-    }
-
-    createRoot(document.getElementById("root")!).render(
-      <StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <UserContext.Provider value={user}>
-            <App />
-          </UserContext.Provider>
-        </QueryClientProvider>
-      </StrictMode>,
-    );
+  .catch((error) => {
+    console.error("Fehler bei der Keycloak-Initialisierung:", error);
+    return false; // Initialisierung fehlgeschlagen, also nicht authentifiziert
   });
+
+if (authenticated) {
+  user = await fetchUser(keycloak.subject!, keycloak.tokenParsed?.name!);
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <UserContext.Provider value={user}>
+        <App />
+      </UserContext.Provider>
+    </QueryClientProvider>
+  </StrictMode>,
+);
